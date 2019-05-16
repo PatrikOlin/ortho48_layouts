aaab5d11fca470b6d449d088fc8d0082c4e6903b
Patrik Olin <enmejl@gmail.com>
2019-05-16 14:30:26 +0200

    added mk4 keymap

--- keymap.c
+++ keymap.c
@@ -49,24 +49,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_ortho_4x12(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUES, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  NO_QUES, KC_ENT,
     KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, TT(_RAISE),   KC_SPC,  KC_SPC,  TT(_LOWER),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  /   |  <>  |      |      |      |      |   _  |   +  |   {  |   }  |  |   |
+ * | Del  |  /   |  <   |   >  |   '  |      |      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |ISO ~ |ISO | |   [  |   ]  |      |
+ * |      |   \  |      |      |   "  |      |      |ISO ~ |ISO | |   [  |   ]  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_SLSH,   KC_NUBS,   _______,   _______,   _______,   _______,   KC_UNDS,    KC_PLUS,    NO_LPRN, NO_RPRN, KC_PIPE,
-    _______, _______,   _______,   _______,   _______,  _______,  _______,  S(KC_NUHS), S(KC_NUBS), KC_LBRC, KC_RBRC,  _______,
+    NO_TILD, KC_EXLM, NO_AT,   KC_HASH, NO_DLR,  KC_PERC, NO_CIRC, NO_AMPR,    NO_ASTR,    NO_LPRN, NO_RPRN, KC_BSPC,
+    KC_DEL,  NO_SLSH,   NO_LESS,   NO_GRTR,   NO_APOS,   NO_GRV,   _______,   NO_UNDS,    NO_PLUS,    NO_LCBR, NO_RCBR, NO_PIPE,
+    _______, NO_BSLS,   _______,   _______,   NO_QUO2,  _______,  _______,  S(KC_NUHS), S(KC_NUBS), NO_LBRC, NO_RBRC,  _______,
     RESET,   _______, _______, _______, KC_TRNS, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
